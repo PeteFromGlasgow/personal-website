b76dba86c31ad8a03e6de2cf8885c7b81b70b508
Peter Kinnaird <peter@kinnaird.scot>
2025-04-22 22:19:08 +0100

    chore: configuration of vue and tailwind

--- packages/frontend/astro.config.mjs
+++ packages/frontend/astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
+import vue from '@astrojs/vue'
+
+import sitemap from '@astrojs/sitemap'
+
+import tailwindcss from '@tailwindcss/vite'
+
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  integrations: [vue(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+})
