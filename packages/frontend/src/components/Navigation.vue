33141bda912c8a794493fa2db944dda2f16a3771
Peter Kinnaird <peter@kinnaird.scot>
2025-04-23 21:49:54 +0100

    feat: added an empty sidebar, ready for content

---
+++ packages/frontend/src/components/Navigation.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarProvider,
+  SidebarTrigger
+} from '@/components/ui/sidebar'
+</script>
+<template>
+<div>
+<SidebarProvider>
+<Sidebar>
+  <SidebarHeader>
+  PeterKinnaird - Engineer
+  </SidebarHeader>
+  <SidebarContent>
+    <SidebarGroup />
+    <SidebarGroup />
+  </SidebarContent>
+  <SidebarFooter />
+</Sidebar>
+<SidebarTrigger></SidebarTrigger>
+</SidebarProvider>
+
+</div>
+</template>
