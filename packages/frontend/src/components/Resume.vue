ccab3a13b5c7cd68b3ceb10cdfb1efd77b29681a
Peter Kinnaird <peter@kinnaird.scot>
2025-05-20 20:27:11 +0100

    feature: Add resume page, right now just spewing json

---
+++ packages/frontend/src/components/Resume.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    {{ contents.basics }} 
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { resume } from '@petefromglasgow/personal-website-resume'
+  type Resume = typeof resume
+  const props = defineProps(['contents'])
+
+  const contents: Resume = props.contents
+</script>
