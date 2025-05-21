16298293edee8517cb331fe2c98b87ad5d56d84c
Peter Kinnaird <peter@kinnaird.scot>
2025-05-21 22:59:49 +0100

    feature: get closer to proper rendering

--- packages/frontend/src/components/Resume.vue
+++ packages/frontend/src/components/Resume.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>
+  <div class="flex flex-col">
     <h1 class="text-4xl pb-2">{{ contents.basics.name }}</h1>
     <h2 class="text-2xl pb-4">About me</h2>
     <p class="italic font-light pl-1">{{ contents.basics.summary }}</p>
     <h2 id='employment' class="text-2xl">Employment</h2>
-    <div v-for="work in contents.work">
-       <p>{{ work.name }} </p>
+    <div class="flex-row flex-none" v-for="work in contents.work">
+      <div class="flex-1 w-52">
+       <p>{{ work.name }}</p>
+      </div>
+      <div class="">
+        {{ work.summary }}
+      </div>
     </div>
 
   </div>
